4ec820d12b3db1aaf32c5ada726853f0c4c78027
João Geonizeli <joao.geonizeli@gmail.com>
2020-03-31 19:31:13 -0300

    add: footer

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 import Navbar from '../../components/Navbar';
+import Footer from '../../components/Footer';
 
 export default function About() {
-  return <Navbar />;
+  return (
+    <>
+      <Navbar />
+      <Footer />
+    </>
+  );
 }
