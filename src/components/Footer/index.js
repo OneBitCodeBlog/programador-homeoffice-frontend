14c90a3c400d25d3c420b39e956dc698c412857a
João Victor <jvsouzax@gmail.com>
2020-03-31 19:31:13 -0300

    add: footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './style.css';
+
+export default function Footer() {
+  return (
+    <footer>
+      <footer>
+        <div className="footer-title">Week Code BootCamp Super Full Stack</div>
+        <div className="footer-copyright">2020 Copyright: One Bit Code</div>
+      </footer>
+    </footer>
+  );
+}
