f24cd9b420ef639c7f57bdeb310dcb8dd7edcb15
João Victor <jvsouzax@gmail.com>
2020-03-31 22:02:43 -0300

    add: messenger icons

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Navbar from '../../components/Navbar';
 import Footer from '../../components/Footer';
+import MessengerIcon from '../../components/MessengerIcon';
 
 export default function About() {
   return (
     <>
       <Navbar />
       <Footer />
+      <MessengerIcon />
     </>
   );
 }
