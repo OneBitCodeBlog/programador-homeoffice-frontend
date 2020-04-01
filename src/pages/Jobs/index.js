725f4c214e98ebcd236ac9684fbda22fbae48e7e
João Geonizeli <joao.geonizeli@gmail.com>
2020-04-01 10:02:14 -0300

    add: messenger icons (#7)

--- src/pages/Jobs/index.js
+++ src/pages/Jobs/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Navbar from '../../components/Navbar';
 import Footer from '../../components/Footer';
+import MessengerIcon from '../../components/MessengerIcon';
 
 export default function Jobs() {
   return (
     <>
       <Navbar />
       <Footer />
+      <MessengerIcon />
     </>
   );
 }
