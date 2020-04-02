ccf0b00d1a6c37ae08efce073a8d46ca01c43fed
João Geonizeli <joao.geonizeli@gmail.com>
2020-04-02 18:40:45 -0300

    fix: about us

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import $ from 'jquery';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
 
 ReactDOM.render(
   <React.StrictMode>
@@ -13,4 +11,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
