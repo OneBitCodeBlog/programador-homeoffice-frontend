c091c67667c8b40fa7977ed65d9a7c3dd12b8563
Lucas Siqueira Fernandes <lucassiqueirafernandes07@gmail.com>
2020-04-02 12:56:08 -0300

    quem somos linking

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+
 
 ReactDOM.render(
   <React.StrictMode>
--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -3,11 +3,13 @@ import React from 'react';
 import Navbar from '../../components/Navbar';
 import Footer from '../../components/Footer';
 import MessengerIcon from '../../components/MessengerIcon';
+import AboutContent from '../../components/AboutContent';
 
 export default function About() {
   return (
     <>
       <Navbar />
+      <AboutContent />
       <Footer />
       <MessengerIcon />
     </>
