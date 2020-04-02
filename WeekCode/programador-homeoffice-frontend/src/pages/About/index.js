e9eeb413680ef43db348e75a31e14ba7be06dbb3
Lucas Siqueira Fernandes <lucassiqueirafernandes07@gmail.com>
2020-04-02 14:05:45 -0300

    refactoring about page folders

--- WeekCode/programador-homeoffice-frontend/src/pages/About/index.js
+++ WeekCode/programador-homeoffice-frontend/src/pages/About/index.js
@@ -3,13 +3,149 @@ import React from 'react';
 import Navbar from '../../components/Navbar';
 import Footer from '../../components/Footer';
 import MessengerIcon from '../../components/MessengerIcon';
-import AboutContent from '../../components/AboutContent';
+import icon from '../../assets/images/github.jpg';
+
+import './style.css';
 
 export default function About() {
   return (
     <>
       <Navbar />
-      <AboutContent />
+
+      <section className="text-center">
+	    <section className="page-header mt-3">
+	    	<h4>Quem Somos</h4>
+	    	<hr />
+	    </section>
+
+	    <section className="container">
+	    	<p>
+		    	Somos uma equipe de startup com o objetivo de centralizar as melhores 
+		    	vagas disponíveis em outras plataformas e apresentá-las de maneira fácil
+		    	e ágil para pessoas em busca de oportunidades.
+		    </p>
+
+		    <p>
+		    	Também contamos com um chatbot de alarmes para anúncios de novas vagas
+		    </p>
+	    </section>
+
+	    <section className="github mt-5">
+	    	<section className="page-header p-3">
+	    		<h4>Colaboradores do Projeto</h4>
+	    		<hr />
+	    	</section>
+
+	    	<section className="list container">
+	    		<section className="row">
+	    			<section className="col-6">
+	    				<a href="https://github.com/AnaSelma">
+			    			- José da Esquina 
+			    			<img src={icon} alt="github icon" />
+		    			</a>
+
+		    			<a href="">
+			    			-  
+			    			<img src={icon} alt="github icon" />
+		    			</a>
+
+		    			<a href="">
+			    			-  
+			    			<img src={icon} alt="github icon" />
+		    			</a>
+
+		    			<a href="">
+			    			-  
+			    			<img src={icon} alt="github icon" />
+		    			</a>
+
+		    			<a href="">
+			    			-  
+			    			<img src={icon} alt="github icon" />
+		    			</a>
+
+		    			<a href="">
+			    			-  
+			    			<img src={icon} alt="github icon" />
+		    			</a>
+
+		    			<a href="">
+			    			-  
+			    			<img src={icon} alt="github icon" />
+		    			</a>
+
+		    			<a href="">
+			    			-  
+			    			<img src={icon} alt="github icon" />
+		    			</a>
+
+		    			<a href="">
+			    			-  
+			    			<img src={icon} alt="github icon" />
+		    			</a>
+
+		    			<a href="">
+			    			-  
+			    			<img src={icon} alt="github icon" />
+		    			</a>
+		    		</section>
+
+		    		<section className="col-6">
+		    			<a href="">
+			    			-  
+			    			<img src={icon} alt="github icon" />
+		    			</a>
+
+		    			<a href="">
+			    			-  
+			    			<img src={icon} alt="github icon" />
+		    			</a>
+
+		    			<a href="">
+			    			-  
+			    			<img src={icon} alt="github icon" />
+		    			</a>
+
+		    			<a href="">
+			    			-  
+			    			<img src={icon} alt="github icon" />
+		    			</a>
+
+		    			<a href="">
+			    			-  
+			    			<img src={icon} alt="github icon" />
+		    			</a>
+
+		    			<a href="">
+			    			-  
+			    			<img src={icon} alt="github icon" />
+		    			</a>
+
+		    			<a href="">
+			    			-  
+			    			<img src={icon} alt="github icon" />
+		    			</a>
+
+		    			<a href="">
+			    			-  
+			    			<img src={icon} alt="github icon" />
+		    			</a>
+
+		    			<a href="">
+			    			-  
+			    			<img src={icon} alt="github icon" />
+		    			</a>
+
+		    			<a href="">
+			    			-  
+			    			<img src={icon} alt="github icon" />
+		    			</a>
+		    		</section>
+	    		</section>
+	    	</section>
+	    </section>	
+	  </section>
+
       <Footer />
       <MessengerIcon />
     </>
