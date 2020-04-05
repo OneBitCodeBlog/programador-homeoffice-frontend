c1500c0d2fdbcd290a27d724f2309bc0651e6ca9
Lucas Siqueira Fernandes <lucassiqueirafernandes07@gmail.com>
2020-04-04 22:27:19 -0300

    altering messenger hover

--- src/components/MessengerIcon/index.js
+++ src/components/MessengerIcon/index.js
@@ -4,6 +4,16 @@ import './style.css';
 
 import icon from '../../assets/images/messenger.png';
 
+function show() {
+  var x = document.getElementsByClassName("messenger_div");
+  x[0].style.display = "block";
+}
+
+function hide() {
+  var x = document.getElementsByClassName("messenger_div");
+  x[0].style.display = "none";
+}
+
 export default function MessengerIcon() {
   return (
     <a
@@ -11,12 +21,17 @@ export default function MessengerIcon() {
       href="https://facebook.com.br"
       target="_blank"
       rel="noopener noreferrer"
-    >
-      <div className="messenger-text">
-        <p>Quer receber atualizações em tempo real no Messenger?</p>
-        <p>Conheça nosso serviço!</p>
+      onMouseEnter={show}
+      onMouseLeave={hide}
+    > 
+      <div className="messenger_div">
+        <div className="messenger-text">
+          <p>Quer receber atualizações em tempo real no Messenger?</p>
+          <p>Conheça nosso serviço!</p>
+        </div>
+        <span />
       </div>
-      <span />
+
       <img src={icon} alt="Messenger Icon" />
     </a>
   );
