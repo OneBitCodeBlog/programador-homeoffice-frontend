abe698a9a3d1d013bc93b54ba3065dae3cfd7042
Lucas Siqueira Fernandes <lucassiqueirafernandes07@gmail.com>
2020-04-05 20:50:49 -0300

    adding names in about us page

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -10,29 +10,26 @@ import './style.css';
 
 export default function About() {
   const profiles = [
-    { name: 'José da Esquina', github: 'https://github.com' },
-    { name: 'José da Esquina', github: 'https://github.com' },
-    { name: 'José da Esquina', github: 'https://github.com' },
-    { name: 'José da Esquina', github: 'https://github.com' },
-    { name: 'José da Esquina', github: 'https://github.com' },
-    { name: 'José da Esquina', github: 'https://github.com' },
-    { name: 'José da Esquina', github: 'https://github.com' },
-    { name: 'José da Esquina', github: 'https://github.com' },
-    { name: 'José da Esquina', github: 'https://github.com' },
-    { name: 'José da Esquina', github: 'https://github.com' },
-    { name: 'José da Esquina', github: 'https://github.com' },
-    { name: 'José da Esquina', github: 'https://github.com' },
-    { name: 'José da Esquina', github: 'https://github.com' },
-    { name: 'José da Esquina', github: 'https://github.com' },
-    { name: 'José da Esquina', github: 'https://github.com' },
-    { name: 'José da Esquina', github: 'https://github.com' },
-    { name: 'José da Esquina', github: 'https://github.com' },
-    { name: 'José da Esquina', github: 'https://github.com' },
-    { name: 'José da Esquina', github: 'https://github.com' },
-    { name: 'José da Esquina', github: 'https://github.com' },
-    { name: 'José da Esquina', github: 'https://github.com' },
-    { name: 'José da Esquina', github: 'https://github.com' },
-    { name: 'José da Esquina', github: 'https://github.com' },
+    { name: 'Adriano Avelino', github: 'https://github.com/adrianoavelino' },
+    { name: 'Guilherme Arruda', github: 'https://github.com/Guiarrd' },
+    
+    { name: 'Ana Selma Alves', github: 'https://github.com/AnaSelma' },
+    { name: 'João Victor Almeida', github: 'https://github.com/aalmeidadev' },
+
+    { name: 'André Oliveira', github: 'https://github.com/oliveira-andre' },
+    { name: 'João Victor Geonizeli', github: 'https://github.com/geonizeli' },
+
+    { name: 'Antonio Cavalcante', github: 'https://github.com/toticavalcanti' },
+    { name: 'José da Esquina', github: 'https://github.com/cheesepaulo' },
+
+    { name: 'Caio Felipe Motta Ladeia', github: 'https://github.com/CaioFML' },
+    { name: 'Luan Soliño', github: 'https://github.com/lsolino' },
+
+    { name: 'Chrystiam Andrade', github: 'https://github.com/chrystiamweb' },
+    { name: 'Lucas Siqueira Fernandes', github: 'https://github.com/Lucasdfg07' },
+
+    { name: 'Diego Gonçalves', github: 'https://github.com/DiegoRamires' },
+    { name: 'Paulo Cesar', github: 'https://github.com' }
   ];
 
   return (
